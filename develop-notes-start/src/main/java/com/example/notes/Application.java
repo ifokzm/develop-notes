3edfa5360be2c383430558e01855ded97de7afd9
朱猛 <zhumeng@znlh.com>
2023-07-28 16:16:07 +0800

    init

--- develop-notes-start/src/main/java/com/example/notes/Application.java
+++ develop-notes-start/src/main/java/com/example/notes/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevelopNotesApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DevelopNotesApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
