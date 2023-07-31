7aff68095a13b9a721b270da859169d88631c462
朱猛 <zhumeng@znlhzl.com>
2023-08-01 00:10:54 +0800

    rocketmq demo

--- develop-notes-web/src/main/java/com/github/sailboat/notes/demos/web/RocketMQOrderCOntroller.java
+++ develop-notes-web/src/main/java/com/github/sailboat/notes/demos/web/RocketMQOrderCOntroller.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -16,7 +17,8 @@ public class RocketMQOrderCOntroller {
      * 发送同步顺序消息
      */
     @RequestMapping(value = "/testSyncOrderSend", method = RequestMethod.GET)
-    public void testSyncSend(){
+    @ResponseBody
+    public String testSyncSend(){
         //参数一：topic   如果想添加tag,可以使用"topic:tag"的写法
         //参数二：消息内容
         //参数三：hashKey 用来计算决定消息发送到哪个消息队列， 一般是订单ID，产品ID等
@@ -27,6 +29,7 @@ public class RocketMQOrderCOntroller {
         mqManager.syncSendOrderly("test-topic-orderly","222222创建","222222");
         mqManager.syncSendOrderly("test-topic-orderly","222222支付","222222");
         mqManager.syncSendOrderly("test-topic-orderly","222222完成","222222");
+        return "成功";
     }
 
 }
